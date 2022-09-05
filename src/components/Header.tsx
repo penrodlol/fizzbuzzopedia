fd02eb6f0fae2d22227b12b1fc5559aecceb3212
penrodlol <christian@penrod.contact>
2022-09-05 00:11:53 -0400

    ♿️ Focus indicators

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,16 +27,20 @@ export const Header = () => {
     <header className="sticky top-0 left-0 right-0 z-50 bg-2">
       <nav className="py-4 px-fluid-4 max-w-screen-2xl mx-auto my-0">
         <ul className="flex items-center justify-between text-xl">
-          <li
-            className="text-fluid-4 font-extrabold text-transparent bg-clip-text
-                       bg-gradient-to-r from-brand-1 to-brand-2"
-          >
+          <li>
             <Route href="/" passHref>
-              Fizzbuzzopedia
+              <a
+                className="rounded-md text-fluid-4 font-extrabold text-transparent bg-clip-text
+                           bg-gradient-to-r from-brand-1 to-brand-2"
+              >
+                Fizzbuzzopedia
+              </a>
             </Route>
           </li>
           <li className="w-9 h-9 text-brand-2">
-            <button onClick={toggle}>{theme && THEMES[theme]}</button>
+            <button className="rounded-md" onClick={toggle}>
+              {theme && THEMES[theme]}
+            </button>
           </li>
         </ul>
       </nav>
